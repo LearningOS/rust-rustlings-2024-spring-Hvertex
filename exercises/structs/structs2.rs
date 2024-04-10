e95fb062f09985d8066de8e1b0c5f26bbff9bef9
lw <lylwself@gmail.com>
2024-04-10 15:32:41 +0800

    finish moules

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -5,17 +5,17 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[derive(Debug)]
 struct Order {
-    name: String,
-    year: u32,
-    made_by_phone: bool,
-    made_by_mobile: bool,
-    made_by_email: bool,
-    item_number: u32,
-    count: u32,
+    pub name: String,
+    pub year: u32,
+    pub made_by_phone: bool,
+    pub made_by_mobile: bool,
+    pub made_by_email: bool,
+    pub item_number: u32,
+    pub count: u32,
 }
 
 fn create_order_template() -> Order {
@@ -30,6 +30,18 @@ fn create_order_template() -> Order {
     }
 }
 
+fn create_my_order() -> Order {
+    Order {
+        name: String::from("Hacker in Rust"),
+        year: 2019,
+        made_by_phone: false,
+        made_by_mobile: false,
+        made_by_email: true,
+        item_number: 123,
+        count: 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,7 +50,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = create_my_order();
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
